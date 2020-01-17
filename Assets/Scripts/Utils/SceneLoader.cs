e1f803a0a1b521b737864c31348e2107fe98f03b
AmadeoClaramunt <amadeocginer@gmail.com>
2020-01-17 18:59:16 +0100

    added main menu

---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Outfit7.Util.LoadScene
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
